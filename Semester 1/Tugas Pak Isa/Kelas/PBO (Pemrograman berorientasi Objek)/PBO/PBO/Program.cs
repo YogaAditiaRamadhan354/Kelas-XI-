b9e83e1a5e0800f113fc3020ce99ce5f73fbdfad
YogaditiaR <129285892+YogaAditiaRamadhan354@users.noreply.github.com>
2026-09-14 14:15:53 +0700

    Add files via upload

---
+++ Semester 1/Tugas Pak Isa/Kelas/PBO (Pemrograman berorientasi Objek)/PBO/PBO/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+namespace Matematika
+{
+    class Home
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Belajar PBO");
+
+            Kalkulator Cal = new Kalkulator();
+            Cal.Penjumlahan();
+            Cal.Pengurangan();
+            Cal.Perkalian();
+            Cal.Pembagian();
+
+            Bangundatar Bdatar = new Bangundatar();
+            Bangundatar.LuasPersegipanjang();
+            Bangundatar.KelilingPersegipanjang();
+            Bangundatar.LuasLingkaran();
+
+            BangunRuang Bruang = new BangunRuang();
+            Bruang.VolumeTabung();
+            Bruang.VolumeBalok();
+            Bruang.VolumePyramid();
+
+
+        }
+        
+        
+    }
+
+
+    class Kalkulator
+    {
+        public void Penjumlahan()
+        {
+            Console.WriteLine("Penjumlahan");
+        }
+
+        public void Pengurangan()
+        {
+            Console.WriteLine("Pengurangan");
+        }
+
+        public void Perkalian()
+        {
+            Console.WriteLine("Perkalian");
+        }
+
+        public void Pembagian() 
+        {
+            Console.WriteLine("Pembagian");
+        }
+    }
+
+    class Bangundatar
+    {
+        public static void KelilingPersegipanjang()
+        {
+            Console.WriteLine("");
+        }
+
+         public static void LuasPersegipanjang()
+        {
+            Console.WriteLine("");
+        }
+
+        public static void LuasLingkaran()
+        {
+            Console.WriteLine("");
+        }
+    }
+
+    class BangunRuang
+    {
+        public void VolumeBalok()
+        {
+            Console.WriteLine("Volume Balok");
+        }
+
+        public void VolumeTabung()
+        {
+            Console.WriteLine("Volume Tabung");
+        }
+
+        public void VolumePyramid()
+        {
+            Console.WriteLine("Volume Pyramid");
+        }
+    }
+}
